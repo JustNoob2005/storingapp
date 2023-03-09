a00eb5f92216d40c568bbcad1457fa24477e8d07
JustNoob2005 <corne.jobse2005@gmail.com>
2023-03-09 11:15:00 +0100

    fixed execute

--- backend/meldingenController.php
+++ backend/meldingenController.php
@@ -4,14 +4,24 @@
 $attractie = $_POST['attractie'];
 $capaciteit = $_POST['capaciteit']; 
 $melder = $_POST['melder'];
-
-echo $attractie . " / " . $capaciteit . " / " . $melder;
+$type = "achtbaan";
 
 //1. Verbinding
 require_once 'conn.php';
 
 //2. Query
-
+$query = "INSERT INTO meldingen (attractie, capaciteit, melder, type)
+Values(:attractie, :capaciteit, :melder, :type)";
 //3. Prepare
-
+$statement = $conn->prepare($query);
 //4. Execute
+$statement->execute([
+    ":attractie" => $attractie,
+    ":capaciteit" => $capaciteit,
+    ":melder" => $melder,
+    ":type" => $type
+]);
+
+header("Location: ../meldingen/index.php?msg=Melding opgeslagen");
+
+?>
